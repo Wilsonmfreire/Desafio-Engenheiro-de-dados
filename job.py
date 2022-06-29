14fb3afaa446bffb0f80c405b9db4d29a23e7add
Wilson Morais Freire <62375886+Wilsonmfreire@users.noreply.github.com>
2022-06-29 13:35:46 -0300

    job corrigido

--- job.py
+++ job.py
@@ -3,17 +3,21 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import *
 import pyspark.sql.functions as func
 
-def __main__():
-    spark = SparkSession.builder.getOrCreate()
-    tTransacao = spark.read.parquet('/Users/wilso/Documents/Desafio/transacoes.parquet')
-    tContrato = spark.read.parquet('/Users/wilso/Documents/Desafio/contrato.parquet')
-
 def ganho_total(transacao, contrato):
     desafio = contrato.join(Transacoes, contrato.client_id == Transacoes.client_id, 'left')\
         .filter(contrato.is_active == True)\
         .select(func.round(sum((col('total_amount')-(col('total_amount')*coalesce(col('discount_percentage'),lit(0.0))/100))*col('percentage'))/100,2).alias('mes')\
         .alias('Fechamento'))
-    #desafio.write.parquet('desafio.parquet')
-    return print(desafio)
+        
+    return desafio
     
-    ganho_total(Transacoes, contrato)
+def __main__():
+
+    spark = SparkSession.builder.getOrCreate()
+
+    tTransacao = spark.read.parquet('/Users/wilso/Documents/Desafio/transacoes.parquet')
+    tContrato = spark.read.parquet('/Users/wilso/Documents/Desafio/contrato.parquet')
+
+    desafio = ganho_total(tTransacao, tContrato)
+
+    print(desafio.collect())

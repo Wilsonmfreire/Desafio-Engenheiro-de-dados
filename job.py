e7331d39f5b89767097d4cfb4d83d6c56208f68a
Wilson Morais Freire <62375886+Wilsonmfreire@users.noreply.github.com>
2022-06-29 12:41:54 -0300

    Add files via upload

---
+++ job.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+import pyspark.sql.functions as func
+
+def __main__():
+    spark = SparkSession.builder.getOrCreate()
+    tTransacao = spark.read.parquet('/Users/wilso/Documents/Desafio/transacoes.parquet')
+    tContrato = spark.read.parquet('/Users/wilso/Documents/Desafio/contrato.parquet')
+
+def ganho_total(transacao, contrato):
+    desafio = contrato.join(Transacoes, contrato.client_id == Transacoes.client_id, 'left')\
+        .filter(contrato.is_active == True)\
+        .select(func.round(sum((col('total_amount')-(col('total_amount')*coalesce(col('discount_percentage'),lit(0.0))/100))*col('percentage'))/100,2).alias('mes')\
+        .alias('Fechamento'))
+    #desafio.write.parquet('desafio.parquet')
+    return print(desafio)
+    
+    ganho_total(Transacoes, contrato)
